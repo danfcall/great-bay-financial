7bbac3f0945eab0652892e09d242650fdf79c894
Dan Callahan <danfcall@gmail.com>
2026-03-29 16:32:37 -0400

    Switch deployment from GitHub Pages to Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
+  const isNetlifyBuild = process.env.NETLIFY === 'true';
+
   return {
-    base: '/great-bay-financial/',
+    base: isNetlifyBuild ? '/' : '/great-bay-financial/',
     server: {
       port: 3000,
       host: '0.0.0.0',
